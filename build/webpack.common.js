2bcc53f651072064d073a20fca39ce7e8aa65a27
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-06-30 23:39:04 -0600

    Update loaders

--- build/webpack.common.js
+++ build/webpack.common.js
@@ -8,26 +8,40 @@ module.exports = {
 		app: `${APP_DIR}/index.jsx`
 	},
 	module: {
-		rules: [
-			{
-				test: /\.jsx?/,
-				include: APP_DIR,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.(sass|scss)$/,
-				include: APP_DIR,
-				loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader')
-			},
-			{
-				test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-				loader: 'url-loader',
-				options: {
-					limit: 10000,
-					name: 'img/[name].[hash:7].[ext]'
-				}
+		rules: [{
+			test: /\.jsx?/,
+			include: APP_DIR,
+			loader: 'babel-loader'
+		},
+		{
+			test: /\.(sass|scss)$/,
+			include: APP_DIR,
+			loader: ExtractTextPlugin.extract('css-loader!postcss-loader!sass-loader')
+		},
+		{
+			test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				name: 'img/[name].[hash:7].[ext]'
 			}
-		]
+		},
+		{
+			test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				name: 'media/[name].[hash:7].[ext]'
+			}
+		},
+		{
+			test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+			loader: 'url-loader',
+			options: {
+				limit: 10000,
+				name: 'media/[name].[hash:7].[ext]'
+			}
+		}]
 	},
 	resolve: {
 		extensions: ['.js', '.jsx', '.scss'],
