8a2044735e7de67c99741a467e75008f5f8f08f7
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-06-30 18:53:30 -0600

    No emit on errors

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -21,7 +21,8 @@ module.exports = merge(common, {
 			filename: 'index.html',
 			template: `${PUBLIC_DIR}/index.html`,
 			title: 'Robot Art'
-		})
+		}),
+		new webpack.NoEmitOnErrorsPlugin()
 	],
 	output: {
 		path: ROOT_DIR,
