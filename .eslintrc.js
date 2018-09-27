d6e5621c72fca8fa86d892829cf32621ef5585c0
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2018-09-27 17:10:31 -0600

    Update lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,21 +25,21 @@ module.exports = {
 	},
 	plugins: ['react', 'import'],
 	rules: {
-		'arrow-parens': ['error', 'as-needed'],
-		'comma-dangle': ['error', 'never'],
-		'comma-spacing': ['error', { 'before': false, 'after': true }],
-		'eol-last': ['error', 'always'],
-		'import/imports-first': ['error', 'absolute-first'],
+		'arrow-parens': [ERROR, 'as-needed'],
+		'comma-dangle': [ERROR, 'never'],
+		'comma-spacing': [ERROR, { 'before': false, 'after': true }],
+		'eol-last': [ERROR, 'always'],
+		'import/imports-first': [ERROR, 'absolute-first'],
 		'import/no-unresolved': ERROR,
-		'indent': ['error', 'tab'],
+		'indent': [ERROR, 'tab'],
 		'linebreak-style': OFF,
-		'max-len': ['error', { code: 100 }],
+		'max-len': [ERROR, { code: 100 }],
 		'no-console': WARN,
 		'no-undef': OFF,
 		'no-unused-vars': ERROR,
-		'object-curly-spacing': ['error', 'always'],
-		'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
-		'react/jsx-curly-spacing': ['error', {
+		'object-curly-spacing': [ERROR, 'always'],
+		'react/jsx-closing-bracket-location': [ERROR, 'line-aligned'],
+		'react/jsx-curly-spacing': [ERROR, {
 			when: 'always',
 			spacing: {
 				objectLiterals: 'never'
@@ -50,8 +50,8 @@ module.exports = {
 		'react/no-array-index-key': OFF,
 		'react/no-unused-prop-types': ERROR,
 		'react/prefer-stateless-function': ERROR,
-		'semi': ['error', 'always'],
-		'quotes': ['error', 'single']
+		'semi': [ERROR, 'always'],
+		'quotes': [ERROR, 'single']
 	},
 	settings: {
 		'import/resolver': {
