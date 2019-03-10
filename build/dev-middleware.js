10cbd7309cfdeb5980930678e5014c858ffe8c26
Dan Zervoudakes <dan.zervoudakes@gmail.com>
2019-03-09 18:58:02 -0700

    Phrasing

--- build/dev-middleware.js
+++ build/dev-middleware.js
@@ -5,6 +5,9 @@ const ora = require('ora');
 const opn = require('opn');
 const webpackConfig = require('./webpack.dev');
 
+const spinner = ora('Compiling for development...');
+spinner.start();
+
 const compiler = webpack(webpackConfig);
 const port = process.env.port || 8080;
 
@@ -17,9 +20,6 @@ const hotMiddleware = webpackHot(compiler, {
 	log: false,
 	heartbeat: 2000
 });
-
-const spinner = ora('Compiling for development...');
-spinner.start();
 
 devMiddleware.waitUntilValid(() => {
 	spinner.stop();
